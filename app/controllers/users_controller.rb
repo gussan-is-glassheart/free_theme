040bf3b0f725f3ca32b7db06e94bc8864fef3f8c
山口義昌 <yamaguchiyoshimasa@yamaguchiyoshimasanoMacBook-Pro.local>
2023-02-03 10:31:23 +0900

    Additional user registration function (provisional)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
+  #skip_before_action :require_login, only: %i(new create)
+  #before_action :require_not_logged_in, only: %i(new create)
+
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #auto_login(@user)
+      redirect_to login_path, success: t('.success')
+    else
+      flash.now[:error] = t('.fail')
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(user).permit(:email, :name, :password, :password_confirmation)
   end
 end
