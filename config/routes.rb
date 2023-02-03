5d25135f96de5fca24f23466b4a2424e13f5a1ce
山口義昌 <yamaguchiyoshimasa@yamaguchiyoshimasanoMacBook-Pro.local>
2023-02-04 08:35:21 +0900

    Additional user profile functions (provisional)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
+# ユーザー登録・ログイン・プロフィール
+  get 'signup', to: 'users#new'
+  post 'signup', to: 'users#create'
+
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
 
-  resources :users, only: %i[new create destroy]
+  resource :profile, only: %i[show edit update destroy]
 end
