d25227c8fcc0eaed932f482555029717f95835f9
山口義昌 <yamaguchiyoshimasa@yamaguchiyoshimasanoMacBook-Pro.local>
2023-02-06 16:22:49 +0900

    Run rails g controller StaticPages

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def top
+  end
+
+  def terms
+  end
+
+  def privacy_policy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/top'
+  get 'static_pages/terms'
+  get 'static_pages/privacy_policy'
 # ユーザー登録・ログイン・プロフィール
   get 'signup', to: 'users#new'
   post 'signup', to: 'users#create'
