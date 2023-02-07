f608f679a6c887cea1de66a7d1141307d347a2b0
山口義昌 <yamaguchiyoshimasa@yamaguchiyoshimasanoMacBook-Pro.local>
2023-02-07 15:09:29 +0900

    Setting validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  has_many :photo_themes
+
+  validates :name, presence: true
 end
--- app/models/photo_theme.rb
+++ app/models/photo_theme.rb
@@ -2,4 +2,7 @@ class PhotoTheme < ApplicationRecord
   belongs_to :category
   has_many :theme_boards, as: :themeable
   serialize :subject, Array
+
+  validates :target, presence: true
+  validates :subject, presence: true
 end
