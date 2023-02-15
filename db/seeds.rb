5e957acbae9cab58676acf9369892eeb69b1d6fe
山口義昌 <yamaguchiyoshimasa@yamaguchiyoshimasanoMacBook-Pro.local>
2023-02-15 14:48:01 +0900

    Creation of sample data for checking (seed)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Category
+Category.create!(name: "建物")
+Category.create!(name: "動物")
+
+# ThemeItems
+ThemeItem.create!(subject: "Tower")
+ThemeItem.create!(subject: "Bridge")
+ThemeItem.create!(subject: "Dog")
+ThemeItem.create!(subject: "Bird")
+ThemeItem.create!(subject: "Rock Dove")
+ThemeItem.create!(subject: "Temple")
+ThemeItem.create!(subject: "Japanese Architecture")
+ThemeItem.create!(subject: "Shrine")
+
+# PhotoThemes
+PhotoTheme.create!(category_id: 1, target: "神社・仏閣")
+PhotoTheme.create!(category_id: 2, target: "犬")
+PhotoTheme.create!(category_id: 1, target: "タワー")
+PhotoTheme.create!(category_id: 2, target: "ハト")
+
+# PhotoThemeItems
+PhotoThemeItem.create!(photo_theme_id: 1, theme_item_id: 6)
+PhotoThemeItem.create!(photo_theme_id: 1, theme_item_id: 7)
+PhotoThemeItem.create!(photo_theme_id: 1, theme_item_id: 8)
+PhotoThemeItem.create!(photo_theme_id: 2, theme_item_id: 3)
+PhotoThemeItem.create!(photo_theme_id: 3, theme_item_id: 1)
+PhotoThemeItem.create!(photo_theme_id: 4, theme_item_id: 4)
+PhotoThemeItem.create!(photo_theme_id: 4, theme_item_id: 5)
