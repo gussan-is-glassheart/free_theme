6b18d1f3f4729a392256029dc18c853317665aba
山口義昌 <yamaguchiyoshimasa@yamaguchiyoshimasanoMacBook-Pro.local>
2023-02-21 21:03:37 +0900

    Additional testing of theme deletion functionality

--- spec/system/theme_boards_spec.rb
+++ spec/system/theme_boards_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "ThemeBoards", type: :system do
   let(:user) { create(:user) }
-  let!(:category) { create(:category) }
+  let(:category) { create(:category) }
   let!(:photo_theme) { create(:photo_theme, category: category) }
 
   before { login_as(user) }
@@ -15,6 +15,16 @@ RSpec.describe "ThemeBoards", type: :system do
         click_button category.name
         expect(page).to have_content I18n.t('theme_boards.create.success')
         expect(page).to have_content "#{photo_theme.target}を撮影しよう"
+      end
+    end
+
+    context '作成したテーマを削除' do
+      it '削除が成功する' do
+        click_link I18n.t('theme_boards.new.title')
+        click_button category.name
+        click_link I18n.t('theme_boards.show.delete')
+        expect(page).to have_content I18n.t('theme_boards.destroy.success')
+        expect(current_path).to eq theme_boards_path
       end
     end
   end
