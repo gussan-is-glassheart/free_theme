6472b4c77334056b22fa7ac1380f3ebcb801b1a6
山口義昌 <yamaguchiyoshimasa@yamaguchiyoshimasanoMacBook-Pro.local>
2023-03-08 20:37:45 +0900

    Testing of profile change and delete functionality

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -130,4 +130,46 @@ RSpec.describe "Users", type: :system do
       end
     end
   end
+
+  describe 'ログイン後' do
+    before do
+      login_as(user)
+      click_link I18n.t('profiles.show.title')
+    end
+
+    describe 'プロフィール変更' do
+      context 'フォームの入力値が正常' do
+        it 'プロフィール変更が成功する' do
+          click_link I18n.t('defaults.edit')
+          fill_in 'user[name]', with: 'new_test_user'
+          click_button I18n.t('defaults.edit')
+          expect(page).to have_content I18n.t('profiles.update.success')
+          expect(page).to have_content 'new_test_user'
+          expect(current_path).to eq profile_path
+        end
+      end
+
+      context 'ユーザー名が未入力' do
+        it 'プロフィールの変更が失敗する' do
+          click_link I18n.t('defaults.edit')
+          fill_in 'user[name]', with: ''
+          click_button I18n.t('defaults.edit')
+          expect(page).to have_content I18n.t('profiles.update.fail')
+          expect(page).to have_content User.human_attribute_name(:name) + I18n.t('errors.messages.blank')
+          expect(current_path).to eq edit_profile_path
+        end
+      end
+    end
+
+    describe 'ユーザー削除' do
+      context 'ユーザー削除のボタンをクリック' do
+        it 'ユーザーが正常に削除される' do
+          click_link I18n.t('defaults.user_delete')
+          expect(page.accept_confirm).to eq I18n.t('defaults.user_delete_confirmation')
+          expect(page).to have_content I18n.t('profiles.destroy.success')
+          expect(current_path).to eq root_path
+        end
+      end
+    end
+  end
 end
