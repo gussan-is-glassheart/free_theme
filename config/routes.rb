022c76ecdc3a2942237fa24b32551dc36a154096
山口義昌 <yamaguchiyoshimasa@yamaguchiyoshimasanoMacBook-Pro.local>
2023-04-09 19:52:06 +0900

    Installation of administrata / deactivation of seeds files / other settings, etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :theme_items
+      resources :theme_boards
+      resources :photo_theme_items
+      resources :photo_themes
+      resources :photo_achievements
+      resources :categories
+
+      root to: "users#index"
+    end
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 # テーマボード
   resources :theme_boards, except: %i[edit] do
